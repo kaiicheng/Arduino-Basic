56215eeabf30dd1c2333d750818456d2b59bc192
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-05-12 23:14:11 +0800

    Add files via upload

---
+++ Sample_4.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+#include <Servo.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(A0));
+}
